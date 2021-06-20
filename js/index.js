74140bdf5051255a7fc15c3d0a9bf268f5aee645
NoNameTitan <86165315+Jsm-Studio@users.noreply.github.com>
2021-06-20 17:27:14 +0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,30 @@
-import { XStudio, XBlank, XSection, XContent, XList, XText, XFooter } from "https://x-titan.github.io/xstudio/index.js"
+import { XStudio, XBlank, XSection, XContent, XList, XText, XHero, XFooter, XImage } from "https://x-titan.github.io/xstudio/index.js"
 globalThis.XText = XText
 const jsm = new XStudio("#app")
 jsm
   .init()
   .then(() =>
     [
-      XSection({},
-        XList({listType: "row", id: "main" },
-          XText({ css: ["name"], tagName: "h1" }, "JSM-Studio"),
-          XBlank({css:"image"})
+      XBlank({ id: "nav_panel", css: "wrapper" },
+        XContent(
+          XBlank({id:"logo"},XHero(XText("JSM"))),
+          XList({ tagName: "ul", listType: "row" },
+            XBlank({ tagName: "li" }, XText({ tagName: "a" }, "index")),
+            XBlank({ tagName: "li" }, XText({ tagName: "a" }, "about")),
+            XBlank({ tagName: "li" }, XText({ tagName: "a" }, "cart")),
+            XBlank({ tagName: "li" }, XText({ tagName: "a" }, "contact"))
+          )
         )
       ),
-      XSection()
+      XSection(
+        XList({ listType: "row", id: "main" },
+          XText({ css: ["name"], tagName: "h1" }, "JSM-Studio"),
+          XBlank({ css: "image" }, XImage("https://titanium-studio.github.io/src/jpg/robot4.jpg"))
+        )
+      ),
+      XSection(
+        XList()
+      )
     ]
   )
   .then(jsm.use)
